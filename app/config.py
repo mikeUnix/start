959ef93a8c73cf4d90f75b45200ce7ec31184664
root <root@localhost.localdomain>
2020-05-09 22:59:32 -0700

    nastroil kali

--- app/config.py
+++ app/config.py
@@ -1,4 +1,5 @@
 class Configuration(object):
 	DEBUG = True
 	SQLALCHEMY_TRACK_MODIFICATIONS = False
-	SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://postgres:1@localhost/test1' #Access denied for user 'root'@'localhost'
+	SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://postgres:1@localhost/test1' #Access denied for user 'root'@'localhost' 
+	#eto ebuchee sobytie otnyalo y menya dohuya vremeni no blya ya nastroil ety huety i mne ponyatno chto moej jizni po hody ne hvatit
