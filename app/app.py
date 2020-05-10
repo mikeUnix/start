24e496acc3d9203d15eedee27dd8461c8506f138
Mike Unix <masteranisimov@gmail.com>
2020-05-10 14:58:08 +0300

    blueprints

--- app/app.py
+++ app/app.py
@@ -1,11 +1,9 @@
 from flask import Flask
 from config import Configuration
 from flask_sqlalchemy import SQLAlchemy 
-from posts.blueprint import posts #name folder, name file import from posts , posts- instanse for Blueprint class
 
 
 app = Flask(__name__)
 app.config.from_object(Configuration)
 
 db = SQLAlchemy(app)
-app.register_blueprint(posts, url_prefix='/blog')
--- app/posts/blueprint.py
+++ app/posts/blueprint.py
@@ -1,7 +1,14 @@
 from flask import Blueprint
 from flask import render_template
+from models import Post
 
 posts = Blueprint('posts', __name__, template_folder = 'templates')
 @posts.route('/') # 
 def index():
-	return render_template('posts/index.html') # return template page index.html posts.index insert from base.html 
+	posts= Post.query.all()
+	return render_template('posts/index.html', posts=posts) # return template page index.html posts.index insert from base.html 
+
+@posts.route('/<slug>')
+def post_detail(slug):
+	post = Post.query.filter(Post.slug==slug).first()
+	return render_template('posts/post_detail.html', post=post)
